4687d855c8ec89e8aa4ae4d0dcf2da9cee7bb217
pepealania <pepealania@users.noreply.github.com>
2026-05-31 01:12:46 +0000

    deploy: 2b4c730c6473cabf206a388de69ea988f8fa5c26

---
+++ _next/static/fhyTOwsQRNjrf0mdwkIiX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/image-processing-project-tutorial-deep-learning-happy-not-happy-classification//_next/:path+",
+        "destination": "/image-processing-project-tutorial-deep-learning-happy-not-happy-classification/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
